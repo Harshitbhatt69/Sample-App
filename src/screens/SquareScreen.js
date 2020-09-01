843b88182a4f4c0871a6fa5a59acb2a38fd4ebb1
Harshit B <harshitbhatt972@gmail.com>
2020-09-02 01:45:51 +0530

    Reusable page (ColorCount) is ready and connected to SquareScreen.js. Remaning: Applications of state variables

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import {Text,View,StyleSheet} from 'react-native';
+import ColorCounter from "../components/ColorCounter";
 
 const SquareScreen = () => {
 
-    return <Text>Square Screen</Text>;
+    return <View>
+    <ColorCounter color="Red" />
+    <ColorCounter color="Blue" />
+    <ColorCounter color="Green" />
+
+    </View>
 };
 
 const styles = StyleSheet.create({});
